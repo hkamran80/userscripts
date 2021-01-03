ffc9abc594cce1bcb166bdac38d6d28a27ea8d95
H. Kamran <hkamran@unisontech.org>
2021-01-03 01:10:20 -0800

    Create canvas_lms.user.js

---
+++ canvas_lms.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Canvas LMS - Keystrokes
+// @namespace    https://hkamran.com
+// @version      1.0.1
+// @description  Snippets for the Canvas LMS
+// @author       H. Kamran
+// @downloadUrl  https://github.com/hkamran80/userscripts/raw/main/canvas_lms.user.js   
+// @updateUrl    https://github.com/hkamran80/userscripts/raw/main/canvas_lms.user.js   
+// @match        https://*.instructure.com/courses/*
+// @grant        none
+// ==/UserScript==
+
+function pagination(e) {
+    var evtobj = window.event? event : e
+    
+    // Module Page Pagination
+    if (evtobj.keyCode == 78 && evtobj.shiftKey) {
+        // Next Page Keystroke (Shift + N)
+        const next_button_element = document.querySelector("span.module-sequence-footer-button--next a")
+        window.location.href = next_button_element.href;
+    } else if (evtobj.keyCode == 80 && evtobj.shiftKey) {
+        // Previous Page Keystroke (Shift + P)
+        const prev_button_element = document.querySelector("span.module-sequence-footer-button--previous a")
+        window.location.href = prev_button_element.href;
+    }
+}
+
+document.onkeydown = pagination;
