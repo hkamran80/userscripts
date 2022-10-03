438c448ba6edb549d448de0bdda6e7bc2f0373f6
H. Kamran <hkamran@unisontech.org>
2022-10-03 08:54:46 -0700

    Add a catch for the comment field being active, switch to arrow function syntax, refactor variable names to be camelCase

--- ao3_keystrokes.user.js
+++ ao3_keystrokes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AO3 - Keystrokes
 // @namespace    https://hkamran.com
-// @version      1.0.4
+// @version      1.0.5
 // @description  Keystrokes for AO3
 // @author       H. Kamran
 // @downloadUrl  https://github.com/hkamran80/userscripts/raw/main/ao3_keystrokes.user.js
@@ -15,48 +15,57 @@
 // @grant        none
 // ==/UserScript==
 
-function work_pagination(e) {
-    var evtobj = window.event ? event : e;
-    if ((evtobj.keyCode === 78 && evtobj.shiftKey) || evtobj.keyCode === 39) {
-        // Next Chapter/Part in Series Keystroke (Shift + N || Right Arrow)
-        const next_button_element =
-            document.querySelector(".chapter.next a") ||
-            document.querySelector(".series .next");
-        window.location.href = next_button_element.href;
-    } else if (
-        (evtobj.keyCode === 80 && evtobj.shiftKey) ||
-        evtobj.keyCode === 37
-    ) {
-        // Previous Chapter/Part in Series Keystroke (Shift + P || Left Arrow)
-        const prev_button_element =
-            document.querySelector(".chapter.previous a") ||
-            document.querySelector(".series .previous");
-        window.location.href = prev_button_element.href;
-    } else if (evtobj.keyCode === 75) {
-        // Kudos Keystroke (K)
-        document.querySelector("form#new_kudo input[type=submit]").click();
-    }
-}
+const workPagination = (e) => {
+    const evtobj = window.event ? event : e;
 
-function collection_pagination(e) {
-    var evtobj = window.event ? event : e;
+    if (
+        document.activeElement.tagName.toLowerCase() !== "textarea" &&
+        document.activeElement.name !== "comment[comment_content]"
+    ) {
+        if (
+            (evtobj.keyCode === 78 && evtobj.shiftKey) ||
+            evtobj.keyCode === 39
+        ) {
+            // Next Chapter/Part in Series Keystroke (Shift + N || Right Arrow)
+            const nextChapterElement =
+                document.querySelector(".chapter.next a") ||
+                document.querySelector(".series .next");
+            window.location.href = nextChapterElement.href;
+        } else if (
+            (evtobj.keyCode === 80 && evtobj.shiftKey) ||
+            evtobj.keyCode === 37
+        ) {
+            // Previous Chapter/Part in Series Keystroke (Shift + P || Left Arrow)
+            const previousChapterElement =
+                document.querySelector(".chapter.previous a") ||
+                document.querySelector(".series .previous");
+            window.location.href = previousChapterElement.href;
+        } else if (evtobj.keyCode === 75) {
+            // Kudos Keystroke (K)
+            document.querySelector("form#new_kudo input[type=submit]").click();
+        }
+    }
+};
+
+const collectionPagination = (e) => {
+    const evtobj = window.event ? event : e;
     if ((evtobj.keyCode === 78 && evtobj.shiftKey) || evtobj.keyCode === 39) {
         // Next Page Keystroke (Shift + N || Right Arrow)
-        const next_button_element = document.querySelector(
+        const nextPageElement = document.querySelector(
             "li.next a[rel=next]"
         );
-        window.location.href = next_button_element.href;
+        window.location.href = nextPageElement.href;
     } else if (
         (evtobj.keyCode === 80 && evtobj.shiftKey) ||
         evtobj.keyCode === 37
     ) {
         // Previous Page Keystroke (Shift + P || Left Arrow)
-        const prev_button_element = document.querySelector(
+        const previousPageElement = document.querySelector(
             "li.previous a[rel=prev]"
         );
-        window.location.href = prev_button_element.href;
+        window.location.href = previousPageElement.href;
     }
-}
+};
 
 if (
     (window.location.href.indexOf("/collections/") !== -1 ||
@@ -64,8 +73,8 @@ if (
     window.location.href.indexOf("/works/") === -1
 ) {
     console.info("[AO3 Keystrokes] Using collection pagination keystrokes");
-    document.onkeydown = collection_pagination;
+    document.onkeydown = collectionPagination;
 } else if (window.location.href.indexOf("/works/") !== -1) {
     console.info("[AO3 Keystrokes] Using work pagination keystrokes");
-    document.onkeydown = work_pagination;
+    document.onkeydown = workPagination;
 }
