93bb83cbac2715ca90fae984e66d6b8d035111e0
H. Kamran <hkamran@hkamran.com>
2023-04-02 03:32:04 -0700

    Add support for studio pages

--- letterboxd_releasedatesorter.user.js
+++ letterboxd_releasedatesorter.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Letterboxd Release Date Sorter
 // @namespace    https://hkamran.com
-// @version      1.0.0
-// @description  Automatically applies the release date (newest first) filter on biography pages
+// @version      1.1.0
+// @description  Automatically applies the release date (newest first) filter on Letterboxd pages
 // @author       H. Kamran
 // @downloadUrl  https://github.com/hkamran80/userscripts/raw/main/letterboxd_releasedatesorter.user.js
 // @updateUrl    https://github.com/hkamran80/userscripts/raw/main/letterboxd_releasedatesorter.user.js
@@ -14,6 +14,7 @@
 // @match        https://letterboxd.com/costumes/*
 // @match        https://letterboxd.com/make-up/*
 // @match        https://letterboxd.com/set-decoration/*
+// @match        https://letterboxd.com/studio/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=letterboxd.com
 // @grant        none
 // ==/UserScript==
@@ -23,6 +24,11 @@
 
     if (!window.location.href.includes("/by/")) {
         console.log("[LRDS] Changing to release date sorting...");
-        window.location.href = window.location.href + "/by/release";
+        if (window.location.href.includes("/page/")) {
+            const splitHref = window.location.href.split("/page");
+            window.location.href = `${splitHref[0]}/by/release/page/{splitHref[1]}`;
+        } else {
+            window.location.href = `${window.location.href}/by/release`;
+        }
     }
 })();
