5c7ad4927cd7831b77eadeec19a8f375c1491520
H. Kamran <hkamran@hkamran.com>
2023-04-05 14:05:58 -0700

    Add support for lists

--- letterboxd_releasedatesorter.user.js
+++ letterboxd_releasedatesorter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Letterboxd Release Date Sorter
 // @namespace    https://hkamran.com
-// @version      1.1.1
+// @version      1.2.0
 // @description  Automatically applies the release date (newest first) filter on Letterboxd pages
 // @author       H. Kamran
 // @downloadUrl  https://github.com/hkamran80/userscripts/raw/main/letterboxd_releasedatesorter.user.js
@@ -15,6 +15,7 @@
 // @match        https://letterboxd.com/make-up/*
 // @match        https://letterboxd.com/set-decoration/*
 // @match        https://letterboxd.com/studio/*
+// @match        https://letterboxd.com/*/list/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=letterboxd.com
 // @grant        none
 // ==/UserScript==
