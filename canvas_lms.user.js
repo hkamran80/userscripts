ec5bf13b921ec842a2eb7ebcc41e2b03f83c7a2f
H. Kamran <hkamran@hkamran.com>
2023-05-10 13:50:16 -0700

    Add check for active element

--- canvas_lms.user.js
+++ canvas_lms.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Canvas LMS - Keystrokes
 // @namespace    https://hkamran.com
-// @version      1.0.2
+// @version      1.0.3
 // @description  Snippets for the Canvas LMS
 // @author       H. Kamran
 // @downloadUrl  https://github.com/hkamran80/userscripts/raw/main/canvas_lms.user.js
@@ -10,26 +10,35 @@
 // @grant        none
 // ==/UserScript==
 
-function pagination(e) {
-    var evtobj = window.event ? event : e;
+/**
+ * The pagination function
+ * @param {Event} e The event object
+ */
+const pagination = (e) => {
+    const event = window.event ? window.event : e;
 
-    // Module Page Pagination
-    if ((evtobj.keyCode === 78 && evtobj.shiftKey) || evtobj.keyCode === 39) {
-        // Next Page Keystroke (Shift + N)
-        const next_button_element = document.querySelector(
-            "span.module-sequence-footer-button--next a"
-        );
-        window.location.href = next_button_element.href;
-    } else if (
-        (evtobj.keyCode === 80 && evtobj.shiftKey) ||
-        evtobj.keyCode === 37
+    if (
+        document.activeElement.tagName.toLowerCase() !== "textarea" &&
+        document.activeElement.tagName.toLowerCase() !== "input"
     ) {
-        // Previous Page Keystroke (Shift + P)
-        const prev_button_element = document.querySelector(
-            "span.module-sequence-footer-button--previous a"
-        );
-        window.location.href = prev_button_element.href;
+        // Module Page Pagination
+        if ((event.keyCode === 78 && event.shiftKey) || event.keyCode === 39) {
+            // Next Page Keystroke (Shift + N, right arrow)
+            const next_button_element = document.querySelector(
+                "span.module-sequence-footer-button--next a"
+            );
+            window.location.href = next_button_element.href;
+        } else if (
+            (event.keyCode === 80 && event.shiftKey) ||
+            event.keyCode === 37
+        ) {
+            // Previous Page Keystroke (Shift + P, left arrow)
+            const prev_button_element = document.querySelector(
+                "span.module-sequence-footer-button--previous a"
+            );
+            window.location.href = prev_button_element.href;
+        }
     }
-}
+};
 
 document.onkeydown = pagination;
