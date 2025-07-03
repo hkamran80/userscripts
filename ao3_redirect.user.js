4f599fb8dd1f545f382370e3f15ee1ad588c1831
H. Kamran <hkamran@hkamran.com>
2025-07-03 00:00:17 -0700

    AO3MR 1.0.1: Add `insecure`

--- ao3_redirect.user.js
+++ ao3_redirect.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         AO3 Mirror Redirect
 // @namespace    https://hkamran.com
-// @version      1.0.0
+// @version      1.0.1
 // @description  Redirect the mirror sites to the main site
 // @author       H. Kamran
 // @downloadUrl  https://github.com/hkamran80/userscripts/raw/main/ao3_redirect.user.js
 // @updateUrl    https://github.com/hkamran80/userscripts/raw/main/ao3_redirect.user.js
 // @match        http*://archive.transformativeworks.org/*
+// @match        http://insecure.archiveofourown.org/*
 // @grant        none
 // ==/UserScript==
 
 (function () {
-  window.location.href = window.location.href.replace(
-    "archive.transformativeworks.org",
-    "archiveofourown.org",
-  );
+  window.location.href = window.location.href
+    .replace("archive.transformativeworks.org", "archiveofourown.org")
+    .replace("insecure.archiveofourown.org", "archiveofourown.org");
 })();
